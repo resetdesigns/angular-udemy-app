501a91af02266f3a243ebc0bd4e4b9f990d90c72
Joseph Villajin <resetdesigns@gmail.com>
2020-08-13 21:39:19 -0700

    Data Binding

--- udemy-app/src/app/servers/servers.component.ts
+++ udemy-app/src/app/servers/servers.component.ts
@@ -2,11 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server>in the middle<app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'test server';
+  userName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! The server name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetName() {
+    this.userName = '';
+  }
 }
